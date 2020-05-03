6830ca19542b5ea4f07e842a20a77cfda68b9a97
Kirill <kvderevyanko@gmail.com>
2020-05-03 08:43:01 +0300

    Второй коммит

---
+++ 13.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * Напишите функцию PHP, чтобы получить самый большой ключ в массиве.
+ *
+ * Пример массива
+ * $array = [2 => 'a', 12 => 'b', 4 => 'f', 1 => 'a'];
+ * function maxKey ($array) {
+ *      ........
+ *      return $maxKey;
+ * }
+ *
+ *
+ * $maxKey =  maxKey ($array);
+ * echo  maxKey; // 12
+ */
