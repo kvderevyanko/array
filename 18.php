9eee3051fa2fa0ba18460300d61de8b0dcc5e2e5
Kirill <kvderevyanko@gmail.com>
2020-05-03 09:30:08 +0300

    Пятый коммит

---
+++ 18.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+ * Написать функцию, которая будет возвращать расширение файла из имени (делать через массивы)
+ * function extension($fileName){
+ *      .....
+ *      return $extension;
+ * }
+ *
+ * extension("image.jpg"); // jpg
+ * extension("Мой документ.пдф"); //пдф
+ * extension("Лето 2019.02.31.gif"); // gif
+ */
